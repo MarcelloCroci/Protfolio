956e77be886a773fff6bbcbfa14a3fec679869dd
MarcelloCroci <marci.croci06@gmail.com>
2025-05-11 16:10:12 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:marcello.croci/Portfolio.git main:gh-pages
+git push -f git@github.com:marcellocroci.github.io/Portfolio.git main:gh-pages
 
 cd -
